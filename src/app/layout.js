f2a5e362f407d9374f3f7a4a629e3c288b6b4469
Yuv <billiondollarone07@gmail.com>
2023-08-17 05:16:16 +0000

    fixed sass, added module

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import "./scss/event.scss"
 import { Poppins } from "next/font/google";
 import Provider from "@/components/provider/Provider";
 import Navigation from "@/components/navigation/Navigation";
