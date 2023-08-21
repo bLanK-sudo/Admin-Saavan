83d4b4fa50a68ccbb861b56158348272763801f2
Manish <142194741+realbLanK993@users.noreply.github.com>
2023-08-21 17:27:52 +0530

    changes to next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
     DJANGO_CLIENT_ID: process.env.DJANGO_CLIENT_ID,
+    PUBLIC_URL: process.env.PUBLIC_URL,
+    GOOGLE_REDIRECT_URI: process.env.GOOGLE_REDIRECT_URI,
   },
   reactStrictMode: true,
   images: {
