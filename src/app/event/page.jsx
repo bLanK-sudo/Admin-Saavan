271c2e19fba4fd3806c5cdc4202a4211728b9cde
Yuv local <billiondollarone07@gmail.com>
2023-08-23 17:45:58 +0530

    Event page preview
    - /event route shows how the event looks like to non-admin users

---
+++ src/app/event/page.jsx
@@ -0,0 +1,67 @@
+"use client";
+
+import EventPageTemplate from "@/components/CreateEvent/EventPageTemplate.jsx";
+import { useEffect, useState } from "react";
+import axios from '@/components/axios'
+import { useEvent } from "@/context/EventContext";
+
+export default function CreateEvent() {
+  const {event} = useEvent()
+  const [resData, setResData] = useState(null)
+  const {
+    location,
+    meet_link,
+    fb_link,
+    ig_link,
+    yt_link,
+    twitter_link,
+    website_links,
+    misc_links,
+    registration_start_date,
+    registration_end_date,
+    date,
+    category,
+    name,
+    description,
+  } = resData ?? {}
+
+  const links = {
+    facebook: fb_link,
+    twitter: twitter_link,
+    instagram: ig_link,
+    website: website_links,
+    misc: misc_links,
+    meet: meet_link,
+    youtube: yt_link,
+  }
+  const dates = {
+    event_start_date: date,
+    event_end_date: '',
+    registration_start_date,
+    registration_end_date,
+  }
+
+  useEffect(() => {
+    axios.get(`events/${event?.id}`)
+    .then((res) => setResData(res.data))
+    .catch((err) => {
+      if(err.response) console.log(err)
+    })
+  }, [event])
+
+  if (resData) {
+      return (
+        <>
+          <EventPageTemplate
+            data={description}
+            eventName={name}
+            links={links}
+            dates={dates}
+            sponsors={[]}
+            venue={location}
+            category={category.id}
+          />
+        </>
+      );
+  }
+}
