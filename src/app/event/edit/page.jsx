11ec6682092303b32557913e02d99bcb0972261d
Manish <142194741+realbLanK993@users.noreply.github.com>
2023-08-24 07:33:26 +0530

    Revert "some changes event.scss and edit event"
    This reverts commit 824b366a583522f94e5957223c8b348ae285bf69.

--- src/app/event/edit/page.jsx
+++ src/app/event/edit/page.jsx
@@ -1,45 +1,51 @@
 "use client";
 
-import { AdminEventPage } from "@/components/CreateEvent/EventPage";
+import { AdminEventPage } from "@/components/CreateEvent/EventPage"
 import axios from "@/components/axios";
 import { useAuth } from "@/context/AuthContext";
-import { useEvent } from "@/context/EventContext";
 import { useEffect, useState } from "react";
 
 const AdminEditEventPage = () => {
-  const { token, status } = useAuth();
-  const { event } = useEvent();
-  const [data, setData] = useState(null);
+  const { token, status } = useAuth()
+  const [data, setData] = useState(null)
 
-  useEffect(() => {
-    setData(event);
-  }, [event]);
-  if (status === "loading") {
-    return (
-      <>
-        <main className="fixed w-screen h-screen inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-          <div className="w-32 h-32 bg-white rounded-full" />
-        </main>
-      </>
-    );
-  }
+    useEffect(() => {
+      axios.get('organizers/event/', {headers: {Authorization: `Bearer ${token?.access_token}`}})
+      .then((res) => setData(res.data))
+      .catch((err) => {
+        if (err.response) console.log(err.response)
+      })
+      },
+      [token, status]
+    )
 
-  if (status === "unauthenticated") {
-    return (
-      <>
-        <div className="flex flex-col gap-2 justify-center items-center min-h-[70vh]">
-          <h1 className="text-4xl font-bold">Unauthenticated</h1>
-          <p>You are not logged in</p>
-        </div>
-      </>
-    );
-  }
-  if (data) {
-    // console.log(data)
-    return (
-      <AdminEventPage auth={{ token, status }} {...data} editEvent={true} />
-    );
-  }
-};
+    if (data) {
+      // console.log(data)
+      return (
+        <AdminEventPage auth={{token, status}} {...data} editEvent={true} />
+      )
+    } else {
+      if (status === "loading") {
+        return (
+          <>
+            <main className="fixed w-screen h-screen inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+              <div className="w-32 h-32 bg-white rounded-full" />
+            </main>
+          </>
+        );
+      }
 
-export default AdminEditEventPage;
+      if (status === "unauthenticated") {
+        return (
+          <>
+            <div className="flex flex-col gap-2 justify-center items-center min-h-[70vh]">
+              <h1 className="text-4xl font-bold">Unauthenticated</h1>
+              <p>You are not logged in</p>
+            </div>
+          </>
+        );
+      }
+    }
+}
+
+export default AdminEditEventPage
