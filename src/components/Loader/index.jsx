e5a8e4f586f97daddbb604e1dab7de8bd3a3605b
Manish <142194741+realbLanK993@users.noreply.github.com>
2023-09-06 08:41:59 +0530

    open and close registrations

--- src/components/Loader/index.jsx
+++ src/components/Loader/index.jsx
@@ -1,11 +1,11 @@
-
-
 const Loader = () => {
-    return (
-        <main className="fixed w-screen h-screen inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-            <div className="w-32 h-32 bg-white rounded-full" />
-        </main>
-    )
-}
+  return (
+    <main className="fixed w-screen h-screen inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+      <div className="w-32 h-32 rounded-full">
+        <img src="/logo.jpg" alt="Logo" className="rounded-full" />
+      </div>
+    </main>
+  );
+};
 
-export default Loader
+export default Loader;
